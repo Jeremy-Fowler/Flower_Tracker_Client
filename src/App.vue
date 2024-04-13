325178ac9697d476ab3ebb3435a5691009e406d8
Jeremy Fowler <jembyfowler@gmail.com>
2024-04-12 19:42:27 -0600

    added random background image

--- src/App.vue
+++ src/App.vue
@@ -13,18 +13,38 @@ export default {
   name: 'App',
   setup() {
     return {
-      appState: computed(() => AppState)
+      appState: computed(() => AppState),
+      get background() {
+        const backgrounds = {
+          1: 'https://images.unsplash.com/photo-1585049593555-70a36652e150?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          2: 'https://images.unsplash.com/photo-1496062031456-07b8f162a322?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          3: 'https://images.unsplash.com/photo-1615744455875-7ad33653e8c4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          4: 'https://images.unsplash.com/photo-1582912220545-456a19c1449f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          5: 'https://images.unsplash.com/photo-1558986889-aa828ae63a6d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          6: '',
+          7: '',
+          8: '',
+          9: '',
+          10: '',
+          11: '',
+          12: '',
+        }
+
+        return `url(${backgrounds[new Date().getMonth() + 1]})`
+      }
     }
   }
 }
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
+
 main {
-  background-image: url("https://images.unsplash.com/photo-1505210644572-b40cdc9c53f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80");
+  background-image: v-bind(background);
   background-size: cover;
   background-position: center;
 }
+
 * {
   font-family: "Raleway", sans-serif;
 }
